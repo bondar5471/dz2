6016f1f92d6779beec1def667d354703e4768996
bondar5471 <bondar54711745@gmail.com>
2018-06-13 13:38:40 +0300

    fix

--- dz2_6.rb
+++ dz2_6.rb
@@ -10,6 +10,7 @@ puts 'Количество товара?'
 quantity = gets.chomp.to_f
 shop_list[name] = { price: price, quantity: quantity, total_price: price * quantity }
 end
+
 summ_chek = 0
 shop_list .each { |_key, value| summ_chek += value[:total_price] }
 p shop_list
